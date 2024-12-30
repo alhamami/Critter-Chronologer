5346872d5ea04129c00a3852c69897fe7a69ec80
alhamami <jalalalhamami@gmail.com>
2024-12-30 23:50:51 +0300

    Implemented ScheduleDTO

--- src/main/java/com/udacity/jdnd/course3/critter/schedule/DTO/ScheduleDTO.java
+++ src/main/java/com/udacity/jdnd/course3/critter/schedule/DTO/ScheduleDTO.java
@@ -1,4 +1,6 @@
-package com.udacity.jdnd.course3.critter.schedule;
+package com.udacity.jdnd.course3.critter.schedule.DTO;
+
+import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
 
 import java.time.LocalDate;
 import java.util.List;
